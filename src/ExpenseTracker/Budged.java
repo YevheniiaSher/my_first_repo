0dbc6b8951e25db5b7e5983a1bb7a715bc59bae7
Yevheniia Sheremet <sheremetjen9@gmail.com>
2026-09-13 19:24:58 +0300

    Update/Delete/Get

--- src/ExpenseTracker/Budged.java
+++ src/ExpenseTracker/Budged.java
@@ -11,16 +11,20 @@ enum Category{
 }
 public class Budged {
 
+    public int id;
     public String description;
     public double amount;
     public String category;
     public String date;
 
-    public Budged(String description, String category, double amount, String date){
+
+    public Budged(int currentId, String description, String category, double amount, String date){
+        this.id = id;
         this.amount = amount;
         this.category = category;
         this.description = description;
         this.date = date;
+
     }
 
     //getters
@@ -28,13 +32,31 @@ public class Budged {
         return date;
     }
     public String getCategory(){
+
         return category;
     }
     public double getAmount(){
+
         return amount;
     }
     public String getDescription(){
+
         return description;
+    }
+    public int getById(){
+        return id;
+    }
+    public void setDate(String date){
+        this.date = date;
+    }
+    public void setCategory(String category){
+        this.category = category;
+    }
+    public void setDescription(String description){
+        this.description = description;
+    }
+    public void setAmount(double amount){
+        this.amount = amount;
     }
 
 }
--- src/ExpenseTracker/Storage.java
+++ src/ExpenseTracker/Storage.java
@@ -1,14 +1,19 @@
 package ExpenseTracker;
 
 import java.io.*;
+import java.util.*;
 
 public class Storage {
-    public static void main (String[] args){
-//        File dataStorage = new File("data.txt");
-//        System.out.println("File Created!");
+    public static void saveExpenses (
+            String date,
+            String category,
+            String description,
+            double amount
+    ){
+        int newId = getNextId();
         //write data
         try (BufferedWriter writer = new BufferedWriter(new FileWriter("data.txt", true))){
-            writer.write("date|category|description|amount");
+            writer.write(newId + "|" + date + "|" + category + "|" + description + "|" + amount);
             writer.newLine();
         }catch (IOException e){
             System.out.println("Error writing to file: " + e.getMessage());
@@ -21,6 +26,158 @@ public class Storage {
             }
         }catch (IOException e) {
             System.out.println("Error reading file:"+ e.getMessage());
+        }
+    }
+    //find expenses by ID
+    public static Budged getById(int id){
+        try(BufferedReader reader = new BufferedReader(new FileReader("data.txt"))){
+            String line;
+            while((line = reader.readLine()) != null){
+                if(line.startsWith("id|")){
+                    continue;
+                }
+                String[]data = line.split("\\|");
+                if(data.length != 5){
+                    continue;
+                }
+                int currentId = Integer.parseInt(data[0]);
+                if (currentId == id){
+                    return new Budged(currentId, data[3], data[2], Double.parseDouble(data[4]), data[1]);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading the file:" + e.getMessage());
+        } return null;
+    }
+    //Get next ID
+    private static int getNextId(){
+        int maxId = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader("data.txt"))){
+            String line;
+            while ((line = reader.readLine()) != null){
+                if(line.startsWith("id|")){
+                    continue;
+                }
+                String[]data = line.split("\\|");
+                if(data.length != 5){
+                    continue;
+                }
+                int currentId = Integer.parseInt(data[0]);
+                if(currentId > maxId){
+                    maxId = currentId;
+                }
+            }
+        }catch (FileNotFoundException e){
+            // File does not exist yet.
+            // First ID will be 1.
+        }catch (IOException e){
+            System.out.println("Error reading the file:" + e.getMessage());
+        }return maxId + 1;
+    }
+    // Update expense by ID
+    public static void updateExpense(
+            int id,
+            String date,
+            String category,
+            String description,
+            double amount) {
+
+        List<String> lines = new ArrayList<>();
+
+        try (BufferedReader reader =
+                     new BufferedReader(new FileReader("data.txt"))) {
+
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+
+                if (line.startsWith("id|")) {
+                    lines.add(line);
+                    continue;
+                }
+
+                String[] data = line.split("\\|");
+
+                if (data.length != 5) {
+                    continue;
+                }
+
+                int currentId = Integer.parseInt(data[0]);
+
+                if (currentId == id) {
+
+                    line = id + "|" +
+                            date + "|" +
+                            category + "|" +
+                            description + "|" +
+                            amount;
+                }
+
+                lines.add(line);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + e.getMessage());
+            return;
+        }
+
+        try (BufferedWriter writer =
+                     new BufferedWriter(new FileWriter("data.txt"))) {
+
+            for (String line : lines) {
+                writer.write(line);
+                writer.newLine();
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error updating file: " + e.getMessage());
+        }
+    }
+    // Delete expense by ID
+    public static void deleteExpense(int id) {
+
+        List<String> lines = new ArrayList<>();
+
+        try (BufferedReader reader =
+                     new BufferedReader(new FileReader("data.txt"))) {
+
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+
+                if (line.startsWith("id|")) {
+                    lines.add(line);
+                    continue;
+                }
+
+                String[] data = line.split("\\|");
+
+                if (data.length != 5) {
+                    continue;
+                }
+
+                int currentId = Integer.parseInt(data[0]);
+
+                if (currentId != id) {
+                    lines.add(line);
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + e.getMessage());
+            return;
+        }
+
+        try (BufferedWriter writer =
+                     new BufferedWriter(new FileWriter("data.txt"))) {
+
+            for (String line : lines) {
+                writer.write(line);
+                writer.newLine();
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error deleting record: " + e.getMessage());
         }
     }
 }
